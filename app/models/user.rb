ea90471e31f5a4a272baf3507ba0695ac9ab0364
Neale <nealetham@tamu.edu>
2024-09-05 18:32:20 -0400

    test: fix user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    validates :first_name, presence: true
+    validates :last_name, presence: true
 #  has_many :posts
 #  has_many :comments
 #add another comment. Yeah
